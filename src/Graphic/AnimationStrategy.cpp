ba46e91d16153ff036bc2ad9b445a30ede72fda5
dwarfman78 <fabien.lecorre@gmail.com>
2014-05-07 17:13:36 +0200

    Update AnimationStrategy.cpp
    clean comments.

--- src/Graphic/AnimationStrategy.cpp
+++ src/Graphic/AnimationStrategy.cpp
@@ -3,14 +3,12 @@ namespace se
 {
     void AnimationStrategy::render(RenderingContext& rc)
     {
-       // Logger log("AnimationStrategy::render::"+se::Utils::toString(this));
 
         // Getting animation context from global context
         AnimationContext& context = rc.getAnimationContext();
 
         if(context.isValid())
         {
-            //log << "Context valid, proceeding with animation";
 
             // Add time between two calls to current time.
             context.addToCurrentTime(context.getClock().restart().asMicroseconds());
@@ -21,7 +19,6 @@ namespace se
             if(diff<=0)
             {
                 // It is time to switch to the next frame of the animation.
-                //log << "Switching to next animation frame with prediction of : " + se::Utils::toString(diff);
 
                 // We may be too late regarding frame display time, so we have to catch up.
                 context.setCurrentTime(diff*-1);
